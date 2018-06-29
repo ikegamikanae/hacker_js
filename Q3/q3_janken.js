b44e1efab3871c222c650df9e6c00a4bf5e0b5fe
ikegamikanae <fujii_kanae@hotmail.com>
2018-06-29 13:00:59 +0900

    add Q3 folder and 'q3' file and move test.html

---
+++ Q3/q3_janken.js
@@ -0,0 +1,55 @@
+/**
+ * 課題３：コンピューターとじゃんけんするプログラムを作ろう
+ * 勝負がつくまでやる
+ */
+
+function rand(m, n) {
+    return m + Math.floor((n - m + 1) * Math.random());
+}
+
+let comp;
+let user;
+let game;
+const janken = ["ぐー", "ちょき", "ぱー"];
+
+do {
+    game = true;
+    comp = rand(0, 2);
+    // ユーザー
+    user = window.prompt("じゃんけん・・・(0:ぐー / 1:ちょき / 2:ぱー )");
+    user = parseInt(user);
+    if (user >= 0 && user <= 2) {
+        let name;
+        console.log("コンピューター：" + janken[comp]);
+        console.log("あなた：" + janken[user]);
+        const result = user - comp;
+        switch (result) {
+            case -2:
+                name = "コンピューター";
+                break;
+            case -1:
+                name = "あなた";
+                break;
+            case 0:
+                name = "あいこでしょ！";
+                game = false;
+                break;
+            case 1:
+                name = "コンピューター";
+                break;
+            case 2:
+                name = "あなた";
+                break;
+        }
+
+        if (game) {
+            console.log(`${name}の勝ち！`)
+        } else {
+            console.log(name);
+        }
+    } else {
+        console.log("0,1,2の数字をいれてね。");
+        game = false;
+    }
+
+} while (!game);
