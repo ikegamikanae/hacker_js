0d06c0e6e8141a2c2b487371e1a6e512a898669a
ikegamikanae <fujii_kanae@hotmail.com>
2018-07-05 14:07:09 +0900

    modify kadai6

--- Q6/q6_highlow.js
+++ Q6/q6_highlow.js
@@ -13,7 +13,6 @@ function rand(m, n) {
 }
 
 let counter = 0;
-let game;
 
 function setMsgNum(str) {
     let num = window.prompt(str, "答えを入力してね");
@@ -28,17 +27,15 @@ let dispMsg;
 let userAns;
 dispMsg = "0-100の数字を入力してね。";
 do {
-    game = false;
     userAns = setMsgNum(dispMsg);
     if (answer < userAns) {
         dispMsg = "もっと下だよ";
     } else if (answer === userAns) {
         dispMsg = "正解！";
-        game = true;
-        break;
+        // break;
     } else {
         dispMsg = "もっと上だよ";
     }
-} while (!game);
+} while (!(answer === userAns));
 
 window.prompt(dispMsg + `・・・${counter}回目で当てました！`);
