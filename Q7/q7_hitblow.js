60ccd95b152e8c7cc19cf0e9702c1f424d2d93fa
ikegamikanae <fujii_kanae@hotmail.com>
2018-07-08 00:23:07 +0900

    mod Q7

--- Q7/q7_hitblow.js
+++ Q7/q7_hitblow.js
@@ -1,11 +1,9 @@
-/**
- * 課題７：hit & blow
- * ランダムに用意された4桁の数字を当てる
- * 正解の数字は0-9、同じ数字は重複しない4桁
- * 入力された4桁の数字の回答と正解を比べて、
- * ・数字と桁位置の両方が同じ：Hit
- * ・数字だけが同じで桁位置が異なる：Blow
- */
+// 課題７：hit & blow
+//  ランダムに用意された4桁の数字を当てる
+//  正解の数字は0-9、同じ数字は重複しない4桁
+//  入力された4桁の数字の回答と正解を比べて、
+//  ・数字と桁位置の両方が同じ：Hit
+//  ・数字だけが同じで桁位置が異なる：Blow
 
 /**
  * ランダムな値を返す
@@ -14,6 +12,50 @@ function rand(m, n) {
     return m + Math.floor((n - m + 1) * Math.random());
 }
 
-function 4 digitNum() {
+/**
+ * n個の要素を持つ配列（値はランダムな１桁の整数）を作成
+ */
+function makeDigitNum(n) {
+    const num = [];
+    for (let i = 0; i < n; i++) {
+        num[i] = rand(0, 9);
+    }
+
+    return num;
 
 }
+let counter = 0;
+
+/**
+ * 回答を確認
+ */
+function chkAns(ans, user) {
+    let chkResult;
+    // 配列のメソッド：indexOfを使って比較
+
+    return chkResult;
+}
+// メッセージ出力＋回答受け取り
+function setMsgNum(str) {
+    let num = window.prompt(str, "答えを入力してね");
+    num = parseInt(num);
+    // 受け取った数値を配列にする
+
+    counter++;
+    return num;
+}
+
+const ans = makeDigitNum[3];
+let user;
+const dispMsg = "4桁の数字を当ててみて。";
+do {
+    user = setMsgNum(dispMsg);
+    // 答えとユーザの回答を比較
+    const result = chkAns(ans, user);
+    // 完全に正解ではない場合→比較結果を出力
+
+    // 完全に正解→ループを抜ける
+
+} while (!(ans === user));
+
+window.prompt(dispMsg + `・・・${counter}回目で当てました！`);
